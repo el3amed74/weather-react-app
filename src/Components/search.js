485e822f46dd66664974fe2477d5a9920a3d03b8
Ahmed Salem <88010609+el3amed74@users.noreply.github.com>
2023-10-04 11:14:36 +0300

    Update search.js

--- src/Components/search.js
+++ src/Components/search.js
@@ -20,32 +20,3 @@ const Search = ({ onSearchChange }) => {
     )
 }
 export default Search;
-// export default function Search() {  (e) => setSearch(e.target.value)
-//     const [search, setSearch] = useState("");
-//     const [currentweather, setweather] = useState([]);
-//     function getValue() {
-//         let apiKey = "dd94f859a0e52d6e4767fddf735f04a7";
-//         let url = `https://api.openweathermap.org/data/2.5/weather?q=${search}&units=Metric&appid=${apiKey}`;
-//         fetch(url).then(res => res.json())
-//             .then(data => setweather(data.main))
-//             .catch((error) => console.error("Error fetching data:", error));
-//         console.log(currentweather);
-//         document.querySelector("#search").value = "";
-//     };
-
-
-//     return (
-//         <>
-//             <form>
-//                 <input type="text" id="search" placeholder="Search" onChange={(e) => setSearch(e.target.value)} />
-//                 <div className="img" onClick={getValue}>
-//                     <img src={searchIcon} alt="search" width="20px" />
-//                 </div>
-//             </form>
-//             {/* {Object.keys(currentweather).length > 0 && (
-//                 <Today data={currentweather} />
-//             )} */}
-//             <Today data={currentweather}/>
-//         </>
-//     )
-// }
